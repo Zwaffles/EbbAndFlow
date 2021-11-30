c918702b8bb39fb1666788dd2feb75042c0d3eef
Andreas Zweifel <andreaszweifel@hotmail.com>
2021-11-30 16:15:36 +0100

    AI Pathfinding
    Not done yet, but almost - it should be good for the prototype stage AND beyond

---
+++ Assets/DebugMover.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AstarPath.active.Scan();
+    }
+
+
+
+
+
+    public class Tower : ScriptableObject
+    {
+
+        public int cost;     
+    }
+
+    [CreateAssetMenu(menuName = "Tower/RangedTower")]
+    public class RangedTower : Tower
+    {
+        public float range;
+    }
+
+    [CreateAssetMenu(menuName = "Tower/Blockade")]
+    public class Blockade : Tower
+    {
+
+    }
+}
