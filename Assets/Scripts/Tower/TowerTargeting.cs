3c54349d1af98a150f18d719ec180846ba5e970a
JesperT Indie21 <jesper.taavola@futuregames.nu>
2021-12-01 14:29:16 +0100

    TowerTargeting multitarget

--- Assets/Scripts/Tower/TowerTargeting.cs
+++ Assets/Scripts/Tower/TowerTargeting.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class TowerTargetting : MonoBehaviour
+public class TowerTargeting : MonoBehaviour
 {
     //placeholder, replace with inherited tower range
     [SerializeField] float towerRange = 5f;
+    [SerializeField] int numberOfTargets = 2;
 
-    private GameObject currentlyTargeted;
+    private List<GameObject> currentlyTargeted = new List<GameObject>();
     private CircleCollider2D rangeCollider;
     private List<GameObject> enemiesWithinRange = new List<GameObject>();
-
 
     void Start()
     {
         rangeCollider = GetComponent<CircleCollider2D>();
-        rangeCollider.radius = towerRange;
+        
+        //currentlyTargeted = new GameObject[numberOfTargets];
     }
 
     void Update()
     {
-        currentlyTargeted = FindClosestEnemy(enemiesWithinRange);
+        rangeCollider.radius = towerRange;
+        currentlyTargeted.Clear();
+        currentlyTargeted.AddRange(FindClosestEnemies(enemiesWithinRange, numberOfTargets));
     }
 
 
@@ -42,7 +46,37 @@ public class TowerTargetting : MonoBehaviour
         }
     }
 
-    // Returns closest GameObject in ArrayList
+    // Returns list of GameObjects sorted by distance
+    public List<GameObject> FindClosestEnemies(List<GameObject> list, int numOfObjects)
+    {
+        List<GameObject> goList = list;
+
+        //returns if no object is in list
+        if(goList.Count == 0)
+        {
+            return goList;
+        }
+
+        //sorts game object list by distance
+        Vector3 position = transform.position;
+        goList = goList.OrderBy(go => Vector3.Distance(go.transform.position, position)).ToList<GameObject>();
+
+        //adds sorted objects within numOfObjects limit to sortedObjects
+        List<GameObject> sortedObjects = new List<GameObject>();
+        for(int i = 0; i < numOfObjects; i++)
+        {
+            if(i < goList.Count) 
+            {
+                sortedObjects.Add(goList[i]);
+            }
+        }
+
+        return sortedObjects;
+    }
+}
+
+/*
+// Returns closest GameObject in ArrayList
     public GameObject FindClosestEnemy(List<GameObject> list)
     {
         GameObject closest = null;
@@ -60,35 +94,4 @@ public class TowerTargetting : MonoBehaviour
         }
         return closest;
     }
-
-}
-
-/*
-// Find the name of the closest enemy
-
-using UnityEngine;
-using System.Collections;
-
-public class ExampleClass : MonoBehaviour
-{
-    public GameObject FindClosestEnemy()
-    {
-        GameObject[] gos;
-        gos = GameObject.FindGameObjectsWithTag("Enemy");
-        GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-        foreach (GameObject go in gos)
-        {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
-            {
-                closest = go;
-                distance = curDistance;
-            }
-        }
-        return closest;
-    }
-}
-*/
+    */
