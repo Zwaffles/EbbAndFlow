7debd220029e4e5a41721205d178660281a4769c
Mattias Gustafsson <matte_gsson@hotmail.com>
2021-12-13 09:33:44 +0100

    BuffManager cleanup

--- Assets/Scripts/BuffManager.cs
+++ Assets/Scripts/BuffManager.cs
@@ -8,18 +8,18 @@ public class BuffManager : MonoBehaviour
     public static BuffManager Instance { get { return instance; } }
     private static BuffManager instance;
     private float healthModifier;
-    private int speedModifier;
-    private int infectedCurrencyModifier;   
+    private float speedModifier;
+    private int currencyModifier;   
 
     [SerializeField] List<InfectedHealthModifier> infectedHealthModifiers;
     [SerializeField] List<InfectedSpeedModifier> infectedSpeedModifiers;
     [SerializeField] List<InfectedCurrencyModifier> infectedCurrencyModifiers;
-    [SerializeField] List<EnemySpawnModifier> enemySpawnModifiers;
+    [SerializeField] List<InfectedSpawnModifier> infectedSpawnModifiers;
 
     List<Tower> healthModifierTowers = new List<Tower>();
     List<Tower> speedModifierTowers = new List<Tower>();
     List<Tower> damageModifierTowers = new List<Tower>();
-    List<Tower> infectedCurrencyModifierTowers = new List<Tower>();
+    List<Tower> currencyModifierTowers = new List<Tower>();
 
     private void Awake()
     {
@@ -50,9 +50,9 @@ public class BuffManager : MonoBehaviour
         {
             damageModifierTowers[k].IncreaseInfectionScore(1);
         }
-        for (int l = 0; l < infectedCurrencyModifierTowers.Count; l++)
+        for (int l = 0; l < currencyModifierTowers.Count; l++)
         {
-            infectedCurrencyModifierTowers[l].IncreaseInfectionScore(1);
+            currencyModifierTowers[l].IncreaseInfectionScore(1);
         }
     }
 
@@ -70,9 +70,10 @@ public class BuffManager : MonoBehaviour
                 damageModifierTowers.Add(tower);
                 break;
             case Tower.ModifierType.Currency:
-                infectedCurrencyModifierTowers.Add(tower);                
+                currencyModifierTowers.Add(tower);                
                 break;
             default:
+                Debug.LogWarning("Tower: ModifierType not found!");
                 break;
         }
     }
@@ -91,12 +92,46 @@ public class BuffManager : MonoBehaviour
                 damageModifierTowers.Remove(tower);
                 break;
             case Tower.ModifierType.Currency:
-                infectedCurrencyModifierTowers.Remove(tower);
+                currencyModifierTowers.Remove(tower);
                 break;
             default:
                 break;
         }
     }
+
+    #region HealthModifier
+
+    public void CalculateHealthModifier()
+    {
+        float healthModifierTotal = 0;
+
+        for (int i = 0; i < healthModifierTowers.Count; i++)
+        {
+            healthModifierTotal += GetTowerHealthModifier(healthModifierTowers[i]);
+        }
+        healthModifier = healthModifierTotal;
+    }
+
+    private float GetTowerHealthModifier(Tower tower)
+    {
+        float towerHealthModifier = 0f;
+
+        for (int i = 0; i < infectedHealthModifiers.Count; i++)
+        {
+            if (tower.GetInfectionScore() >= infectedHealthModifiers[i].InfectionScoreTrigger)
+            {
+                towerHealthModifier = infectedHealthModifiers[i].HealthModifier;
+            }
+        }
+        return towerHealthModifier;
+    }
+
+    public float GetHealthModifier()
+    {
+        return healthModifier;
+    }
+
+    #endregion
 
     #region Currency/Spawn-Modifier
 
@@ -104,11 +139,11 @@ public class BuffManager : MonoBehaviour
     {
         int currencyModifierTotal = 0;
 
-        for (int i = 0; i < infectedCurrencyModifierTowers.Count; i++)
+        for (int i = 0; i < currencyModifierTowers.Count; i++)
         {
-            currencyModifierTotal += GetTowerInfectedCurrencyModifier(infectedCurrencyModifierTowers[i]);
+            currencyModifierTotal += GetTowerInfectedCurrencyModifier(currencyModifierTowers[i]);
         }
-        infectedCurrencyModifier = currencyModifierTotal;
+        currencyModifier = currencyModifierTotal;
         return currencyModifierTotal;
     }
 
@@ -130,16 +165,16 @@ public class BuffManager : MonoBehaviour
     {
         List<GameObject> enemiesToSpawn = new List<GameObject>();
 
-        for (int i = 0; i < infectedCurrencyModifierTowers.Count; i++)
+        for (int i = 0; i < currencyModifierTowers.Count; i++)
         {
             List<GameObject> tempList = new List<GameObject>();
 
-            for (int j = 0; j < enemySpawnModifiers.Count; j++)
+            for (int j = 0; j < infectedSpawnModifiers.Count; j++)
             {
-                if (infectedCurrencyModifierTowers[i].GetInfectionScore() >= enemySpawnModifiers[j].InfectionScoreTrigger - 1)
+                if (currencyModifierTowers[i].GetInfectionScore() >= infectedSpawnModifiers[j].InfectionScoreTrigger - 1)
                 {
                     tempList.Clear();
-                    tempList = enemySpawnModifiers[j].EnemiesToSpawn;
+                    tempList = infectedSpawnModifiers[j].EnemiesToSpawn;
                 }
             }
             enemiesToSpawn.AddRange(tempList);
@@ -153,91 +188,36 @@ public class BuffManager : MonoBehaviour
 
     #endregion
 
-    #region HealthModifier
-
-    public void CalculateHealthModifier()
-    {
-        float healthModifierTotal = 0;
-
-        for (int i = 0; i < healthModifierTowers.Count; i++)
-        {
-            healthModifierTotal += GetTowerHealthModifier(healthModifierTowers[i]);
-        }
-        healthModifier = healthModifierTotal;
-    }
-
-    float GetTowerHealthModifier(Tower tower)
-    {
-        float towerHealthModifier = 0f;
-
-        for (int i = 0; i < infectedHealthModifiers.Count; i++)
-        {
-            if (tower.GetInfectionScore() >= infectedHealthModifiers[i].InfectionScoreTrigger)
-            {
-                towerHealthModifier = infectedHealthModifiers[i].HealthModifier;
-            }
-        }
-        return towerHealthModifier;
-    }
-
-    public float GetHealthModifier()
-    {
-        return healthModifier;
-    }
-
-    #endregion
-
     #region SpeedModifier
 
-    public void CalculateIncreasedSpeed()
+    public void CalculateSpeedModifier()
     {
         float totalSpeedIncrease = 0f;
 
         for (int i = 0; i < speedModifierTowers.Count; i++)
         {
-            totalSpeedIncrease += GetIncreasedEnemySpeed(speedModifierTowers[i]);
+            totalSpeedIncrease += GetTowerSpeedModifier(speedModifierTowers[i]);
         }
     }
 
-    float GetIncreasedEnemySpeed(Tower tower)
+    private float GetTowerSpeedModifier(Tower tower)
     {
-        float increasedEnemySpeedModifier = 0f;
+        float towerSpeedModifier = 0f;
 
         for (int i = 0; i < infectedSpeedModifiers.Count; i++)
         {
             if (tower.GetInfectionScore() >= infectedSpeedModifiers[i].InfectionScoreTrigger)
             {
-                increasedEnemySpeedModifier = infectedSpeedModifiers[i].SpeedModifier;
+                towerSpeedModifier = infectedSpeedModifiers[i].SpeedModifier;
             }
         }
-        return increasedEnemySpeedModifier;
+        return towerSpeedModifier;
     }
 
-    float GetSlowEnemySpeed(Tower tower)
+    public float GetSpeedModifier()
     {
-        float slowedEnemySpeedModifier = 0;
-
-        for (int i = 0; i < infectedSpeedModifiers.Count; i++)
-        {
-            if (tower.GetInfectionScore() >= infectedSpeedModifiers[i].InfectionScoreTrigger)
-            {
-                slowedEnemySpeedModifier = infectedSpeedModifiers[i].SpeedModifier;
-            }
-        }
-        return slowedEnemySpeedModifier;
+        return speedModifier;
     }
 
     #endregion
-
-    
-
-    public void IncreaseEnemyMovementSpeed()
-    {
-
-    }
-
-    public void SlowEnemyMovementSpeed()
-    {
-
-    }
 }
--- Assets/Scripts/Infected Towers/InfectedSpawnModifier.cs
+++ Assets/Scripts/Infected Towers/InfectedSpawnModifier.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System;
 
 [Serializable]
-public class EnemySpawnModifier
+public class InfectedSpawnModifier
 {
     public string stage = "Stage 1";
     [SerializeField] private int infectionScoreTrigger;
