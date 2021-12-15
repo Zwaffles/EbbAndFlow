feb318298b9683b80504572fb7f536d2d20f97e6
Mattias Gustafsson <matte_gsson@hotmail.com>
2021-12-15 09:31:51 +0100

    GameManager References

--- Assets/Scripts/Util/GameManager.cs
+++ Assets/Scripts/Util/GameManager.cs
@@ -7,6 +7,25 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get { return instance; } }
     private static GameManager instance;
 
+    private SelectionManager selectionManager;
+    private InfectionManager infectionManager; 
+    private BuildingManager buildingManager;
+    private SceneManagement sceneManagement;
+    private BuffManager buffManager;
+    private PlayerCurrency playerCurrency;
+    private CheatDetection cheatDetection;
+    private WaveSpawner waveSpawner;
+
+    public SelectionManager SelectionManager { get; }
+    public InfectionManager InfectionManager { get; }
+    public BuildingManager BuildingManager { get; }
+    public SceneManagement SceneManagement { get; }
+    public BuffManager BuffManager { get; }
+
+    public PlayerCurrency PlayerCurrency { get; }
+    public CheatDetection CheatDetection { get; }
+    public WaveSpawner WaveSpawner { get; }
+
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -18,6 +37,17 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(this);
             instance = this;
         }
+
+        /* Managers lmao */
+        selectionManager = FindObjectOfType<SelectionManager>();
+        infectionManager = FindObjectOfType<InfectionManager>();
+        buildingManager = FindObjectOfType<BuildingManager>();
+        sceneManagement = FindObjectOfType<SceneManagement>();
+        buffManager = FindObjectOfType<BuffManager>();
+
+        playerCurrency = FindObjectOfType<PlayerCurrency>();
+        cheatDetection = FindObjectOfType<CheatDetection>();
+        waveSpawner = FindObjectOfType<WaveSpawner>();
     }
 
     public bool CanBuy(int cost)
